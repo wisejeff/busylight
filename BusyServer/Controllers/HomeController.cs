a4e4889e660539df46d7b5e7bce6af379f8d1098
jeffwise <jeff.l.wise@gmail.com>
2020-05-29 13:39:17 -0400

    initial commit

---
+++ BusyServer/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace BusyServer.Controllers
+{
+    
+    public class HomeController:Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
---
+++ BusyServer/Controllers/StatusController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace BusyServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController: ControllerBase
+    {
+        private readonly ILogger<StatusController> logger;
+        private readonly IMemoryCache memoryCache;
+
+        public StatusController(ILogger<StatusController> logger, IMemoryCache memoryCache)
+        {
+            this.logger = logger;
+            this.memoryCache = memoryCache;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return memoryCache.Get<string>("busy_status");
+        }
+
+        [HttpPost]
+        public string Set([FromBody]string busyStatus)
+        {
+            var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(24));
+
+            this.memoryCache.Set("busy_status", busyStatus);
+            return Get();
+        }
+    }
+}
